dc9b6a02b24ef6c1e1860e72d33b076c294b84fe
hartzcc <hartzcc@gmail.com>
2019-11-27 09:25:17 -0800

    Simplified code down to the min.  Generating a submission file for kaggle so understand how the submission process works

--- hp_model.py
+++ hp_model.py
@@ -1,63 +1,63 @@
 import tensorflow as tf
-import keras
+from keras.models import Sequential
+from keras.layers import Dense
 import pandas as pd
 from sklearn import preprocessing
-import numpy as np
 
-# Keras model for learning Kaggle Housing prices
+# Callback function
+DESIRED_ACCURACY = 0.99
 
-def hp_model():
+# Read in Data.
+data_dir = '/home/me/Data_Source/hp/data/'
+train_data = data_dir + 'train.csv'
+test_data = data_dir + 'test.csv'
 
-    DESIRED_ACCURACY = 0.99
+# Remove non-numerical data (Don't know how to handle categorical data yet)
+train_df = pd.read_csv(train_data)
+train_df = train_df.select_dtypes(include=['int64'])
+train_df = train_df.dropna()
 
-    # Callback Training Function. Limits tensorflow training to 99% to speed up iterations
+test_df = pd.read_csv(test_data)
+test_df = train_df.select_dtypes(include=['int64'])
+test_df = test_df.dropna()
 
-    class MyCallBack(keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs=None):
-            if logs is None:
-                logs = {}
-            if logs.get('acc') > DESIRED_ACCURACY:
-                print(f"chr(10) Accuracy = {DESIRED_ACCURACY}. Exiting")
-                self.model.stop_training = True
+# Normalize Data
+train_df = train_df.astype(float)
+min_max_scalar = preprocessing.MinMaxScaler()
+x_scaled = min_max_scalar.fit_transform(train_df)
+train_df_normalized = pd.DataFrame(x_scaled)
 
-    callback = MyCallBack()
+test_df = test_df.astype(float)
+x_scaled = min_max_scalar.fit_transform(test_df)
+test_df_normalized = pd.DataFrame(x_scaled)
 
-    # Read in Data.
-    data_dir = '/home/me/Data_Source/hp/data/'
-    train_data = data_dir + 'train.csv'
-    test_data = data_dir + 'test.csv'
+# Break out solutions.  Column 34 for train_df_normalized is the price we are trying predict.  It is normalized so
 
-    # Remove non-numerical data (Don't know how to handle categorical data yet)
-    train_df = pd.read_csv(train_data)
-    train_df = train_df.select_dtypes(include=['int64'])
+y_train_df = train_df_normalized[34]
+y_test_df = test_df.pop('SalePrice')
 
-    test_df = pd.read_csv(test_data)
-    test_df = train_df.select_dtypes(include=['int64'])
+# Compile NN
+model = Sequential()
+model.add(Dense(8, input_dim=35, activation='relu'))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(0.001), metrics=['mse'])
 
-    # Normalize Data
-    train_df = train_df.astype(float)
-    min_max_scalar = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scalar.fit_transform(train_df)
-    train_df_normalized = pd.DataFrame(x_scaled)
+x = train_df_normalized.to_numpy()
+y = y_train_df.to_numpy()
 
-    test_df = test_df.astype(float)
-    x_scaled = min_max_scalar.fit_transform(test_df)
-    test_df_normalized = pd.DataFrame(x_scaled)
+x_test = test_df_normalized.to_numpy()
 
-    print(train_df_normalized)
+model.fit(x, y, epochs=10, batch_size=10)
 
+predictions = model.predict(x_test)
 
+predictions = pd.DataFrame(predictions, columns=['SalePrice'])
+predictions.index.name='Id'
+predictions = (720100.00 - 34900.00) * predictions + 34900
 
+# summarize the first 5 cases
+# to get the original value back you need Max - Min * value + Min or (($720,100 - $34,900) * value) + $34,900.
+predictions.to_csv('/home/me/Data_Source/hp/data/predictions.csv', index=True)
 
-
-
-
-
-    return
-
-
-
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    hp_model()
+print(predictions.head())
+print(y_test_df.head())
